85f8a3119ea948504f34c0689224f21811c96c41
bhaskarmantralahub <BhaskarMantrala@icloud.com>
2026-03-13 00:31:59 -0700

    fix: light theme as default

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { ThemeProvider } from 'next-themes';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="data-theme" defaultTheme="dark" storageKey="vibe_theme">
+    <ThemeProvider attribute="data-theme" defaultTheme="light" storageKey="vibe_theme">
       {children}
     </ThemeProvider>
   );
